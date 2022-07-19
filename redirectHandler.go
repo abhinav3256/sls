a830d6f87f1ebe30d080db8519a99b922c9e3885
abhinav3256 <abhinavkumar3256@gmail.com>
2022-07-19 15:37:30 +0530

    adding new code

--- redirectHandler.go
+++ redirectHandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,21 +15,33 @@ func redirectHandler(c *gin.Context) {
 
 	fmt.Println(id)
 	idd, _ := strconv.Atoi(id)
-	long_url := getLongURL(idd)
+	long_url, expire_at := getLongURL(idd)
+	var currentTime = time.Now().UTC()
+	expire_time, _ := time.Parse(time.RFC3339, expire_at)
+	if currentTime.After(expire_time) {
+		res := gin.H{
+			"success": false,
+			"message": "Page Expire",
+		}
+		c.JSON(http.StatusNotFound, res)
+		c.Abort()
+		return
+	}
 
 	c.Redirect(http.StatusMovedPermanently, long_url)
 
 }
 
-func getLongURL(id int) string {
+func getLongURL(id int) (string, string) {
 
 	long_url := ""
-	SQL := `SELECT "long_link" FROM "link" WHERE id=$1`
+	expire_at := ""
+	SQL := `SELECT "expire_at","long_link" FROM "link" WHERE id=$1`
 
 	rows := DB.QueryRow(SQL, id)
 
-	rows.Scan(&long_url)
+	rows.Scan(&expire_at, &long_url)
 	fmt.Println(long_url)
 
-	return long_url
+	return long_url, expire_at
 }
